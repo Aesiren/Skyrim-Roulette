3dd98e1ce7b4df135c8d5e50db8c875e6b06242d
Evan <Evan@BONKPC>
2016-10-27 17:35:24 -0400

    Add project files.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblDesc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(115, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "New Character";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(112, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Click Button to make new character!";
+            // 
+            // lblDesc
+            // 
+            this.lblDesc.AutoSize = true;
+            this.lblDesc.Location = new System.Drawing.Point(112, 109);
+            this.lblDesc.Name = "lblDesc";
+            this.lblDesc.Size = new System.Drawing.Size(63, 13);
+            this.lblDesc.TabIndex = 2;
+            this.lblDesc.Text = "Description:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 326);
+            this.Controls.Add(this.lblDesc);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Skyrim Roulete";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDesc;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //create each component of the character
+            string Race;
+            string Faction;
+            string Playstyle;
+            string Description;
+            string Character;
+            Random x = new Random();
+            int y;
+
+            string[] Race_Num = new string[10] { "Breton", "Bosmer", "Redguard", "Argonian", "Orc", "Altmer", "Khajit", "Imperial", "Nord", "Dunmer" };
+
+            string[] Fact_Num = new string[10] { "Companions", "Mage's College", "Dark Brotherhood", "Thieves Guild", "Dawnguard", "Vampire", "Blade", "Imperial Army", "Stormcloak", "Bard's College" };
+
+            string[] Play_Num = new string[10] { "Wizard", "Knght", "Ranger", "Barbarian", "Spellsword", "Shaman", "Necromancer", "Assassin", "Paladin", "Bard" };
+
+            string[] PNDesc = new string[10] { "Destruction, Restoriation, and Alteration. Can't use armor. Enchanting focus", "Heavy Armor, One-Handed, and Blocking or Two-Handed and Archery. Can't use magiv. Smithing focus.", "Light armor, One-Handed, and Archery. Can't use heavy armor. Alchemy focus.", "Two-Handed or Dual Wielding, Light Armor, and Archery. Can't use magiv or heavy armor. Alchemy focus.", "Conuration, One-Handed or Two-Handed, and Destruction. Can't use non-conjured weapons. Enchanting focus.", "Restoration, One-Handed, and Destruction. Can't user heavy armor. Alchemy focus.", "Conjuration, Destruction, and Alteration. Can't use restoration. Alchemy focus.", "Sneak, Illusion, and One-Handed or Archery. Can;t use heavy armor. Enchantin focus.", "Heavy Armor, Restoration, and One-Handed or Two-Handed. Can;t use magic other than Restoration. Smithing focus.", "Illusion, Restoration, and One-Handed. Can;t use destruction magic. Enchanting focus." };
+
+            y = x.Next(0, 9);
+            Race = Race_Num[y];
+            y = x.Next(0, 9);
+            Faction = Fact_Num[y];
+            y = x.Next(0, 9);
+            Playstyle = Play_Num[y];
+            Description = PNDesc[y];
+
+            Character = Race + " PLAYER, " + Playstyle + " of the " + Faction;
+            label1.Text = Character;
+            lblDesc.Text = Description;
+
+
+
+
+
+        }
+    }
+}
